c1d55e5b20db35071b56c776a71f1f2f2f98cef7
developomp <developomp@gmail.com>
2021-11-23 11:38:03 +0900

    removed react-tooltip

--- src/renderer/components/BottomBar.tsx
+++ src/renderer/components/BottomBar.tsx
@@ -1,53 +1,73 @@
-import { Menu, Container, Icon } from "semantic-ui-react"
-import ReactTooltip from "react-tooltip"
+import { Menu, Container, Icon, Popup } from "semantic-ui-react"
 
 const BottomBar = () => {
 	return (
 		<Menu fixed="bottom" inverted>
 			<Container>
-				<Menu.Item data-tip="Settings">
-					<Icon link size="large" name="settings" />
-					<ReactTooltip effect="solid" />
-				</Menu.Item>
-				<Menu.Item data-tip="Help">
-					<Icon link size="large" name="question circle" />
-					<ReactTooltip effect="solid" />
-				</Menu.Item>
+				<Popup
+					inverted
+					content="Settings"
+					trigger={
+						<Menu.Item>
+							<Icon link size="large" name="settings" />
+						</Menu.Item>
+					}
+				/>
+
+				<Popup
+					inverted
+					content="Help"
+					trigger={
+						<Menu.Item>
+							<Icon link size="large" name="question circle" />
+						</Menu.Item>
+					}
+				/>
 
 				{/* Separate links that opens a new tab in the browser */}
+
 				<Menu.Menu position="right">
-					<a
-						href="https://mocha-downloader.github.io"
-						target="_"
-						data-tip="Documentation"
-					>
-						<Menu.Item>
-							<Icon link size="large" name="book" />
-							<ReactTooltip effect="solid" />
-						</Menu.Item>
-					</a>
+					<Popup
+						inverted
+						content="Documentation"
+						trigger={
+							<a
+								href="https://mocha-downloader.github.io"
+								target="_"
+							>
+								<Menu.Item>
+									<Icon link size="large" name="book" />
+								</Menu.Item>
+							</a>
+						}
+					/>
 
-					<a
-						href="https://discord.gg/aQqamSCUcS"
-						target="_"
-						data-tip="Discord"
-					>
-						<Menu.Item>
-							<Icon link size="large" name="discord" />
-							<ReactTooltip effect="solid" />
-						</Menu.Item>
-					</a>
+					<Popup
+						inverted
+						content="Discord"
+						trigger={
+							<a href="https://discord.gg/aQqamSCUcS" target="_">
+								<Menu.Item>
+									<Icon link size="large" name="discord" />
+								</Menu.Item>
+							</a>
+						}
+					/>
 
-					<a
-						href="https://github.com/Mocha-Downloader/mocha-downloader"
-						target="_"
-						data-tip="Source Code"
-					>
-						<Menu.Item>
-							<Icon link size="large" name="github" />
-							<ReactTooltip effect="solid" />
-						</Menu.Item>
-					</a>
+					<Popup
+						inverted
+						content="Source Code"
+						trigger={
+							<a
+								href="https://github.com/Mocha-Downloader/mocha-downloader"
+								target="_"
+							>
+								<Menu.Item>
+									<Icon link size="large" name="github" />
+								</Menu.Item>
+							</a>
+						}
+					/>
 				</Menu.Menu>
 			</Container>
 		</Menu>
