197e03b178b449e9a9b4fedeb020425889970273
developomp <developomp@gmail.com>
2021-11-30 17:13:42 +0900

    added youtube playlist download feature

--- src/main/platforms/youtube.com.ts
+++ src/main/platforms/youtube.com.ts
@@ -1,4 +1,5 @@
 import ytdl from "ytdl-core"
+import ytpl from "ytpl"
 import fs from "fs"
 
 import { Platform, PlatformMeta } from "common/constants"
@@ -11,21 +12,19 @@ const meta: PlatformMeta = {
 	code: "yt",
 }
 
-// todo: download as mp3
-
 // enum ChannelVideoSortEnum {
 // 	RECENT = "RECENT",
 // 	OLDEST = "OLDEST",
 // 	POPULAR = "POPULAR",
 // }
 
-enum PlaylistVideoSortEnum {
-	RECENT_ADDED = "RECENT_ADDED",
-	OLDEST_ADDED = "OLDEST_ADDED",
-	POPULAR = "POPULAR",
-	RECENT_PUBLISHED = "RECENT_PUBLISHED",
-	OLDEST_PUBLISHED = "OLDEST_PUBLISHED",
-}
+// enum PlaylistVideoSortEnum {
+// 	RECENT_ADDED = "RECENT_ADDED",
+// 	OLDEST_ADDED = "OLDEST_ADDED",
+// 	POPULAR = "POPULAR",
+// 	RECENT_PUBLISHED = "RECENT_PUBLISHED",
+// 	OLDEST_PUBLISHED = "OLDEST_PUBLISHED",
+// }
 
 /**
  * Converts byte to megabyte.
@@ -54,7 +53,7 @@ async function downloadVideo(url: string): Promise<void> {
 
 	ytdl.getBasicInfo(url).then((videoInfo) => {
 		updateDownloadCard("title", videoInfo.videoDetails.title)
-		const thumbnail = videoInfo.videoDetails.thumbnail.thumbnails[0].url
+		const thumbnail = videoInfo.videoDetails.thumbnails[0].url
 		updateDownloadCard("thumbnail", thumbnail)
 	})
 
@@ -78,25 +77,42 @@ async function downloadVideo(url: string): Promise<void> {
 	video.pipe(fs.createWriteStream(filePath))
 }
 
+/**
+ * Download videos from playlist.
+ *
+ * @param {string} url - URL of the playlist
+ * @param {number[]} selected - index of playlist videos to download starting from 0.
+ *
+ * @returns {Promise<void>}
+ */
 async function downloadPlaylist(
 	url: string,
 	selected: number[]
 ): Promise<void> {
-	console.log(url, selected)
+	const playlist = await ytpl(url, { limit: Infinity })
+
+	selected.map((playlistIndex) => {
+		downloadVideo(playlist.items[playlistIndex].shortUrl)
+	})
 }
 
-async function getPlaylistVideos(url: string, sort: PlaylistVideoSortEnum) {
-	console.log(url, sort)
-	return []
+/**
+ * Gets a list of all items in a playlist
+ *
+ * @param {string} url - URL of the playlist
+ *
+ * @returns {Promise<ytpl.Item[]>}
+ */
+async function getPlaylistVideos(url: string): Promise<ytpl.Item[]> {
+	const playlist = await ytpl(url, { limit: Infinity })
+
+	return playlist.items
 }
 
 async function logic(downloadPayload: DownloadPayload) {
 	const playlistLogic = async () => {
 		if (!downloadPayload.selected || downloadPayload.selected.length <= 0) {
-			getPlaylistVideos(
-				downloadPayload.url,
-				PlaylistVideoSortEnum.RECENT_ADDED
-			).then((playlistData) => {
+			getPlaylistVideos(downloadPayload.url).then((playlistData) => {
 				m2r({
 					type: "select",
 					payload: {
