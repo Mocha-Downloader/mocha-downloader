0e86c47ec4799551b07a20a29548866c452af723
developomp <developomp@gmail.com>
2021-12-10 11:57:51 +0900

    moved dropzone to separate component

--- src/renderer/TabPanes/DownloadPane.tsx
+++ src/renderer/TabPanes/DownloadPane.tsx
@@ -1,40 +1,15 @@
-import {
-	CSSProperties,
-	useCallback,
-	useContext,
-	useEffect,
-	useMemo,
-	useState,
-} from "react"
+import { useContext, useEffect, useState } from "react"
 import { Card, Header } from "semantic-ui-react"
 import { useTranslation } from "react-i18next"
-import { useDropzone } from "react-dropzone"
 import styled from "styled-components"
 
 import { globalContext } from "../ipc"
 
-import AnimatedLogo from "../components/AnimatedLogo"
-import DownloadCard from "../components/DownloadCard"
-import DebuggingInput from "../components/DebuggingInput"
 import { StyledPaneContainer } from "../components/Tabs"
-
-const baseStyle: CSSProperties = {
-	flex: 1,
-	cursor: "pointer",
-	userSelect: "none",
-	display: "flex",
-	flexDirection: "column",
-	alignItems: "center",
-	padding: "20px",
-	borderWidth: 2,
-	borderRadius: 2,
-	borderColor: "#eeeeee",
-	borderStyle: "dashed",
-	backgroundColor: "#fafafa",
-	color: "#bdbdbd",
-	outline: "none",
-	transition: "border .24s ease-in-out",
-}
+import DebuggingInput from "../components/DebuggingInput"
+import DownloadCard from "../components/DownloadCard"
+import AnimatedLogo from "../components/AnimatedLogo"
+import Dropzone from "../components/DropZone"
 
 const StyledInstructions = styled.div`
 	display: flex;
@@ -50,62 +25,6 @@ const DownloadPane = () => {
 	useEffect(() => {
 		setDownloadCardsCount(Object.keys(globalState.downloadCards).length)
 	}, [globalState])
-
-	/**
-	 * Drag 'n' drop
-	 */
-
-	const onDrop = useCallback((acceptedFiles: File[]) => {
-		acceptedFiles.forEach((file) => {
-			const reader = new FileReader()
-
-			reader.onabort = () => {
-				console.log("file reading was aborted")
-			}
-
-			reader.onerror = () => {
-				console.log("file reading has failed")
-			}
-
-			reader.onload = () => {
-				const fileContent: string = reader.result as string
-
-				// todo: user feedback for invalid file, empty file, and unreadable file
-				if (!fileContent) return
-
-				// todo: start batch download
-				const batchDownloadData = JSON.parse(fileContent)
-
-				console.log(batchDownloadData)
-			}
-
-			reader.readAsText(file)
-		})
-	}, [])
-
-	const {
-		getRootProps,
-		getInputProps,
-		isDragActive,
-		isDragAccept,
-		isDragReject,
-	} = useDropzone({
-		onDrop,
-
-		// accept json and commented json files only
-		accept: ".json,.jsonc",
-	})
-
-	// todo: investigate color indication not working properly
-	const style = useMemo(
-		() => ({
-			...baseStyle,
-			...(isDragActive && { borderColor: "#2196f3" }),
-			...(isDragAccept && { borderColor: "#00e676" }),
-			...(isDragReject && { borderColor: "#ff1744" }),
-		}),
-		[isDragActive, isDragReject, isDragAccept]
-	)
 
 	return (
 		<StyledPaneContainer>
@@ -144,10 +63,7 @@ const DownloadPane = () => {
 
 			<br />
 
-			<div {...getRootProps({ style })}>
-				<input {...getInputProps()} />
-				<p>{t("instructions.drop")}</p>
-			</div>
+			<Dropzone />
 		</StyledPaneContainer>
 	)
 }
---
+++ src/renderer/components/DropZone.tsx
@@ -0,0 +1,90 @@
+/**
+ * @file zone for file drag and drop
+ */
+
+import { CSSProperties, useCallback, useMemo } from "react"
+import { useTranslation } from "react-i18next"
+import { useDropzone } from "react-dropzone"
+
+const baseStyle: CSSProperties = {
+	flex: 1,
+	cursor: "pointer",
+	userSelect: "none",
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	padding: "20px",
+	borderWidth: 2,
+	borderRadius: 2,
+	borderColor: "#eeeeee",
+	borderStyle: "dashed",
+	backgroundColor: "#fafafa",
+	color: "#bdbdbd",
+	outline: "none",
+	transition: "border .24s ease-in-out",
+}
+
+const DropZone = () => {
+	const { t } = useTranslation()
+
+	const onDrop = useCallback((acceptedFiles: File[]) => {
+		acceptedFiles.forEach((file) => {
+			const reader = new FileReader()
+
+			reader.onabort = () => {
+				console.log("file reading was aborted")
+			}
+
+			reader.onerror = () => {
+				console.log("file reading has failed")
+			}
+
+			reader.onload = () => {
+				const fileContent: string = reader.result as string
+
+				// todo: user feedback for invalid file, empty file, and unreadable file
+				if (!fileContent) return
+
+				// todo: start batch download
+				const batchDownloadData = JSON.parse(fileContent)
+
+				console.log(batchDownloadData)
+			}
+
+			reader.readAsText(file)
+		})
+	}, [])
+
+	const {
+		getRootProps,
+		getInputProps,
+		isDragActive,
+		isDragAccept,
+		isDragReject,
+	} = useDropzone({
+		onDrop,
+
+		// accept json and commented json files only
+		accept: ".json,.jsonc",
+	})
+
+	// todo: investigate color indication not working properly
+	const style = useMemo(
+		() => ({
+			...baseStyle,
+			...(isDragActive && { borderColor: "#2196f3" }),
+			...(isDragAccept && { borderColor: "#00e676" }),
+			...(isDragReject && { borderColor: "#ff1744" }),
+		}),
+		[isDragActive, isDragReject, isDragAccept]
+	)
+
+	return (
+		<div {...getRootProps({ style })}>
+			<input {...getInputProps()} />
+			<p>{t("instructions.drop")}</p>
+		</div>
+	)
+}
+
+export default DropZone
