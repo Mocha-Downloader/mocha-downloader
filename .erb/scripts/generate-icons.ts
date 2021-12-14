e40d21f532aef9938254430e1e566da7a1974bb7
developomp <developomp@gmail.com>
2021-12-14 16:21:09 +0900

    add icons to windows executable

--- .erb/scripts/generate-icons.ts
+++ .erb/scripts/generate-icons.ts
@@ -1,13 +1,16 @@
-import sharp from "sharp"
-import fs from "fs"
+import { copyFileSync, readFileSync, writeFileSync } from "fs"
 import toIco from "to-ico"
+import sharp from "sharp"
 
 async function main() {
 	const original = sharp("assets/icon.svg", {
 		raw: { height: 1024, width: 1024, channels: 4 },
 	})
 
-	original.toFile("assets/icon.png")
+	original.toFile("assets/icon.png").then(() => {
+		// for electron build
+		copyFileSync("assets/icon.png", "release/build/icon.png")
+	})
 
 	const sizes = [16, 24, 32, 48, 64, 96, 128, 256, 512, 1024]
 	const imageFiles: Buffer[] = []
@@ -20,13 +23,13 @@ async function main() {
 			await resizedImage.toFile(fileName)
 
 			if ([16, 24, 32, 48, 64, 128, 256].includes(size)) {
-				const imageData = fs.readFileSync(fileName)
+				const imageData = readFileSync(fileName)
 				imageFiles.push(imageData)
 
 				if (size === 16)
 					toIco(imageData, { sizes: [16], resize: false }).then(
 						(result) => {
-							fs.writeFileSync("assets/favicon.ico", result)
+							writeFileSync("assets/favicon.ico", result)
 						}
 					)
 			}
@@ -34,7 +37,7 @@ async function main() {
 	)
 
 	toIco(imageFiles).then((result) => {
-		fs.writeFileSync("assets/icon.ico", result)
+		writeFileSync("assets/icon.ico", result)
 	})
 }
 
